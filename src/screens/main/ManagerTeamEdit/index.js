f45a0836ab43d72d2db30e72032db79676f5f53d
tsduc <tsduc0996@gmail.com>
2023-03-23 00:31:26 +0700

    Duc commit

---
+++ src/screens/main/ManagerTeamEdit/index.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Template from "../../../components/Template";
+
+const ManagerTeamEdit = () => {
+  return <Template>Manager Team Edit</Template>;
+};
+
+export default ManagerTeamEdit;
